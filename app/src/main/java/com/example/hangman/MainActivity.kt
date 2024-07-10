0ce1f6ef4a7d2dee27278d2260a6c07137f999fb
Benedict <BenedictH2468@gmail.com>
2024-07-10 10:53:27 +0100

    first commit

---
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.hangman
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hangman.ui.theme.HangmanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HangmanTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Hangman(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Hangman(modifier: Modifier = Modifier) {
+    Column {
+        Text(text = "Lives")
+        TextField(text = lives,)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HangmanTheme {
+        Hangman()
+    }
+}
+
+fun SelectWord() {
+    val lineList = mutableListOf<String>()
+    //File
+}
