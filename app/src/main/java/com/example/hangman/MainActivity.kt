dac74890aa18fdd47e943909b45a94d256a1ab25
Benedict <BenedictH2468@gmail.com>
2024-07-12 10:02:26 +0100

    word generation and underscoring

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hangman
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,9 +12,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.hangman.ui.theme.HangmanTheme
+import java.io.File
+import java.io.BufferedReader
+import java.io.FileWriter
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     Hangman(
                         modifier = Modifier.padding(innerPadding)
                     )
+                    SelectWord()
                 }
             }
         }
@@ -35,7 +47,8 @@ class MainActivity : ComponentActivity() {
 fun Hangman(modifier: Modifier = Modifier) {
     Column {
         Text(text = "Lives")
-        TextField(text = lives,)
+        //TextField(text = currentLives,
+            //onValueChange = { newValue -> currentLives = newValue})
     }
 }
 
@@ -47,7 +60,37 @@ fun GreetingPreview() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
 fun SelectWord() {
-    val lineList = mutableListOf<String>()
-    //File
+    val words = stringResource(R.string.words).split(" ").toTypedArray()
+    val randomIndex = Random.nextInt(words.size);
+    val word = words[randomIndex]
+    Text(text=word)
+}
+
+
+//generates the word to be shown according to the letters guessed so far
+@Preview
+@Composable
+fun GenerateUnderscores(word:String="canal",guessedLetters:String="al"){
+    val builder = StringBuilder()
+    for (char in word){
+        if (char in guessedLetters) {
+            builder.append(char)
+        } else {
+            builder.append("_")
+        }
+    }
+    Text(text=builder.toString())
+    //return builder.toString()
+}
+
+@Preview()
+@Composable
+fun HangmanLetter(letter:String="e") {
+    //var currentLetter by remember {
+    //    mutableStateOf("_")
+    //}
+    //TextField(value = currentLetter, onValueChange = {newLetter -> currentLetter = newLetter})
 }
